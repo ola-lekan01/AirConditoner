821eb01eaf39507ad8da0a01f8f75e38d50b381d
Ola Lekan <lekan.sofuyi01@gmail.com>
2022-06-30 04:21:23 +0100

    The first commit for Unicorn Oil And Gas Testing Script

--- tests/tddTesting/UnicornOilAndGas.java
+++ tests/tddTesting/UnicornOilAndGas.java
@@ -1,2 +1,32 @@
-package tddTesting;public class UnicornOilAndGas {
+package tddTesting;
+
+import testsClass.UniCornOilAndGasTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UnicornOilAndGas {
+
+    @Test
+
+    void testThatObjectExists(){
+        UniCornOilAndGasTest unicornOilAndGas = new UniCornOilAndGasTest(2 , "Yaba");
+        assertNotNull(unicornOilAndGas);
+    }
+
+    @Test
+    void testThatFuelCanBeBought(){
+        UniCornOilAndGasTest unicornOilAndGas = new UniCornOilAndGasTest(2 , "Yaba");
+        unicornOilAndGas.buyFuel();
+        assertTrue(true);
+    }
+
+    @Test
+    void testForPriceAfterDiscount(){
+        UniCornOilAndGasTest unicornOilAndGas = new UniCornOilAndGasTest(2 , "Yaba");
+        unicornOilAndGas.getDiscount(2);
+        assertEquals(392 , unicornOilAndGas.getPrice());
+
+    }
+
 }
