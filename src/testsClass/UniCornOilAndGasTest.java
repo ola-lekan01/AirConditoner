8c477b118b4f9bfdff5d3b112380163d1858f5e4
Ola Lekan <lekan.sofuyi01@gmail.com>
2022-06-30 04:18:32 +0100

    This is the first commit for UniCorn Oil and Gas Testing

---
+++ src/testsClass/UniCornOilAndGasTest.java
@@ -0,0 +1,27 @@
+package testsClass;
+
+public class UniCornOilAndGasTest {
+
+    private String location;
+    private final double fuelPerLitre = 200;
+    private int litre;
+    private double price;
+
+    public UniCornOilAndGasTest(int litre , String location){
+        this.litre = litre;
+        this.location = location;
+    }
+
+    public boolean buyFuel() {
+        return true;
+    }
+
+    public void getDiscount(int litre) {
+        this.price = (fuelPerLitre * litre) - (fuelPerLitre * litre * 0.02);
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+}
+
